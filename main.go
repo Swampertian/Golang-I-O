113b0753aa8a6888ad85d6d19a306649c5ca4965
Pedro Lucas <pedrolmoreira99@gmail.com>
2025-12-05 12:21:12 -0300

    Import log and middleware

--- main.go
+++ main.go
@@ -8,12 +8,19 @@ import (
 
 	"fire-go/internal/db"
 	"fire-go/internal/handlers"
+	"fire-go/internal/logger"
+	"fire-go/internal/middleware"
 )
 
 func main() {
+
 	db.Connect()
+	logger.Init()
+
+	logger.Log.Info("server_starting", "port", 8081)
 
 	r := chi.NewRouter()
+	r.Use(middleware.RequestLogger)
 	r.Get("/fire_intersect", handlers.GetFireIntersectFiltered)
 
 	log.Println("API rodando em :8081")
