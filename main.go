e2fc31e16cca2948c184aadddd5060976d0c1c71
Pedro Lucas <pedrolmoreira99@gmail.com>
2025-12-15 12:14:05 -0300

    Adicionando handler e playground go gqlgen

--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/go-chi/chi/v5"
 
+	"fire-go/graph"
 	"fire-go/internal/db"
 	"fire-go/internal/handlers"
 	"fire-go/internal/logger"
@@ -16,12 +19,22 @@ func main() {
 
 	db.Connect()
 	logger.Init()
+
 	logger.Log.Info("server_starting", "port", 8081)
+
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: graph.NewResolver()}))
 
 	r := chi.NewRouter()
 	r.Use(middleware.RequestLogger)
+
+	// Rotas REST
 	r.Get("/fire_intersect", handlers.GetFireIntersectFiltered)
 
+	// Rotas GraphQL
+	r.Handle("/graphql", srv)
+	r.Handle("/", playground.Handler("GraphQL playground", "/graphql"))
+
 	log.Println("API rodando em :8081")
+	log.Println("GraphQL playground disponível em http://localhost:8081/")
 	http.ListenAndServe(":8081", r)
 }
